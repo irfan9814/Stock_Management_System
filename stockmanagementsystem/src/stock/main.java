827e84a0fad9d55548276a4372b8b6d72c611347
irfan9814 <74260414+irfan9814@users.noreply.github.com>
2023-02-17 22:42:20 +0530

    Add files via upload

--- stockmanagementsystem/src/stock/main.java
+++ stockmanagementsystem/src/stock/main.java
@@ -145,7 +145,8 @@ public class main extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
+        Vendor1 v = new Vendor1();
+        v.setVisible(true);       // TODO add your handling code here:
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
